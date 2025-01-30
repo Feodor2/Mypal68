eaec3f26078e0a388992efe60f30853b148e86b2
Fedor <fedor@mail.ru>
2025-01-30 22:10:53 +0200

    68.14.7 new version

--- browser/branding/unofficial/pref/mypal-branding.js
+++ browser/branding/unofficial/pref/mypal-branding.js
@@ -28,3 +28,5 @@ pref("app.update.badgeWaitTime", 0);
 // Number of usages of the web console or scratchpad.
 // If this is less than 5, then pasting code into the web console or scratchpad is disabled
 pref("devtools.selfxss.count", 5);
+
+pref("general.useragent.override.challenges.cloudflare.com", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0");
